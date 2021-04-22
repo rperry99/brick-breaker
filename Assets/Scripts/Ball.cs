baa33e5a33d3a8f1e8cf6bc3fad29b7863c0829f
Russ <rperry1886@gmail.com>
2021-04-22 19:23:34 -0400

    Lock the ball to the paddle
    At the start of the game, the ball will be locked to the paddle until you shoot it

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // Configuration Parameters
+    [SerializeField] Paddle paddle1;
+
+    // State
+    Vector2 paddleToBallVector;
+
+    // Start is called before the first frame update
+    void Start() {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddleToBallVector + paddlePosition;
+    }
+}
