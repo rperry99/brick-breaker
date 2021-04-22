6a4b8946915b769e09a8915ab73fc7148b1c0952
Russ <rperry1886@gmail.com>
2021-04-22 19:41:24 -0400

    Refactor ball lock and add ball launch
    Convert the code that locks the ball to the paddle into a function. Add a new function that launches the ball on LMB click

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,9 +6,12 @@ public class Ball : MonoBehaviour {
 
     // Configuration Parameters
     [SerializeField] Paddle paddle1;
+    [SerializeField] float velocityX = 5f;
+    [SerializeField] float velocityY = 15f;
 
-    // State
+    // State Variables
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start() {
@@ -17,7 +20,25 @@ public class Ball : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if(!hasStarted){
+            LockBallToPaddle();
+            LaunchBallOnClick();
+        }
+    }
+
+    private void LockBallToPaddle() {
+        // This will lock the ball to the paddle at the start of the round.
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddleToBallVector + paddlePosition;
+    }
+
+    private void LaunchBallOnClick() {
+        // When the user clicks the LMB, then the ball will be launched.
+
+        // We use 0 here as 0 is often tied to the LMB
+        if(Input.GetMouseButtonDown(0)) {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, velocityY);
+        }
     }
 }
